827776b543a444365114e0f9e591f15d937caff9
thanford <thanford7@gmail.com>
2021-11-03 05:12:40 -0400

    Fix base path issue

--- up/up/frontend/js/modules/pages/about/AboutPage.vue
+++ up/up/frontend/js/modules/pages/about/AboutPage.vue
@@ -6,7 +6,7 @@
             hire which cost months of time trying to train and improve the employee. The employee's resume was stellar and all hiring managers liked them so the outcome
                 came as a surprise.</p>
             <p>Todd never wanted to go through that experience again so he developed a project interview which required candidates to analyze data
-            for a made up company, create a presentation of the findings, and present their analysis to a panel of hiring managers.</p>
+            for a fictional company, create a presentation of the findings, and present their analysis to a panel of hiring managers.</p>
             <p>The results were fantastic! The hires made after implementing the project interview were great fits and strong "A performers". Surprisingly,
                 initial impressions and great resumes were not necessarily an indicator of a successful candidate.</p>
             <p>At Uprove, we want to help everyone conduct project based interviews so they can make the best hire possible!</p>
