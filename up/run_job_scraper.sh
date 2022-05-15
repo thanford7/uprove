7854dc983a8614d92be4fa06adbc37a56cc8fa60
thanford <thanford7@gmail.com>
2022-05-15 10:11:05 -0600

    Update Docker script for job scraper

--- up/run_job_scraper.sh
+++ up/run_job_scraper.sh
@@ -3,7 +3,7 @@
 while true;
 do
   echo "Running job scraper" &&
-  python up.upapp.scraper.scraper.runSpiders.py &&
+  python ./upapp.scraper.scraper.runSpiders.py &&
   echo "Sleeping for 4 hours" &&
   sleep 14400;
 done
