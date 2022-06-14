2c89435d15b743b49ee1877c680b1aa2d9460978
thanford <thanford7@gmail.com>
2022-06-14 10:52:25 -0600

    Error logging

--- up/up/frontend/js/vueMixins.js
+++ up/up/frontend/js/vueMixins.js
@@ -265,6 +265,9 @@ const ajaxRequestMixin = {
             // subclass
         },
         onSaveFailure(xhr, textStatus, errorThrown) {
+            console.log(xhr);
+            console.log(textStatus);
+            console.log(errorThrown);
             store.commit('addAlert', {
                 message: this.getFailureMessage(errorThrown, xhr),
                 alertType: SEVERITY.DANGER
